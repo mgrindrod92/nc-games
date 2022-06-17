aa069df6974f9270ea423f0e87df335884ea3c08
mgrindrod92 <matt@grindrod.me.uk>
2022-06-17 15:09:09 +0100

    Beginning of design of adding a comment

--- src/Components/AddComment.js
+++ src/Components/AddComment.js
@@ -2,7 +2,38 @@ import { useState, useEffect } from 'react';
 import { PostComment } from '../Api';
 
 const AddComment = () => {
-    
+    const [commentBody, setCommentBody] = useState('');
+    const [posted, setPosted] = useState(false);
+
+    const handleSubmitComment = (event) => {
+        event.preventDefault();
+        setPosted(true);
+
+    };
+
+    useEffect(() => {
+        const newComment = {
+            body: commentBody,
+            username: `mallionaire`
+        }
+
+        if (posted) {
+            PostComment(review_id, newComment)
+            .then(() => {
+                alert("Your comment has been added. Please refresh the page to view your comment")
+            })
+            .then(() => {
+                setCommentBody('');
+            })
+        }
+    }, [posted, review_id]);
+
+    return (
+        <div className="AddComment">
+            <h3>Submit a comment below</h3>
+            <form className="AddComment__form"></form>
+        </div>
+    )
 }
 
 export default AddComment;
