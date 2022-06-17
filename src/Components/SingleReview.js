8dd164f039d073aa9a14292d760249194028b170
mgrindrod92 <matt@grindrod.me.uk>
2022-06-17 09:00:54 +0100

    Delete duplicate code in SingleReview

--- src/Components/SingleReview.js
+++ src/Components/SingleReview.js
@@ -22,44 +22,6 @@ const SingleReview = (props) => {
     }, [review_id])
     // Or dependent on currentReview?
 
-    const handleUpVote = () => {
-        setVoteChange((currVotes) => currVotes + 1);
-        setCurrentReview((currReview) => {
-            let copyOfReview = { ...currReview };
-            copyOfReview.votes += 1;
-            return copyOfReview;
-        })
-        PatchVotes(review_id, 1)
-            // In case the API request fails
-            .catch(() => {
-                setVoteChange((currVotes) => currVotes - 1);
-                setCurrentReview((currReview) => {
-                    let copyOfReview = { ...currReview }
-                    copyOfReview.votes -= 1;
-                    return copyOfReview;
-                })
-            })
-    }
-
-    const handleDownVote = () => {
-        setVoteChange((currVotes) => currVotes - 1);
-        setCurrentReview((currReview) => {
-            let copyOfReview = { ...currReview };
-            copyOfReview.votes -= 1;
-            return copyOfReview;
-        })
-        PatchVotes(review_id, -1)
-            // In case the API request fails
-            .catch(() => {
-                setVoteChange((currVotes) => currVotes + 1);
-                setCurrentReview((currReview) => {
-                    let copyOfReview = { ...currReview };
-                    copyOfReview.votes += 1;
-                    return copyOfReview;
-                })
-            })
-    }
-
     return (
         <div className="singleReviewSection">
             {/* <li  className="singleReview"> */}
